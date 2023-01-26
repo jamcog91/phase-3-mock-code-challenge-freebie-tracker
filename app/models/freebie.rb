2fa492c920d3e9a318d42a82cb3f3f593910c257
James Cogan <james.m.cogan@gmail.com>
2023-01-26 15:20:01 -0500

    commit tree ebf913cd9e12f36e6c517cd719036cd710918aa8

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,11 @@
 class Freebie < ActiveRecord::Base
+belongs_to :company
+belongs_to :dev
+
+# should return a string formatted as follows: 
+def print_details
+"#{dev.name} owns a #{item_name}
+ from #{company.name}"
+end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,9 @@ puts "Creating freebies..."
 # ***************************************************************
 # * TODO: create freebies! Remember, a freebie belongs to a dev *
 # * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+# ***************************************************************   
+Freebie.create(item_name: "hat", value: 10, dev_id: 1, company_id: 1)
+Freebie.create(item_name: "mug", value: 15, dev_id: 2, company_id: 2)
+Freebie.create(item_name: "t-shirt", value: 25, dev_id: 3, company_id: 3)
 
 puts "Seeding done!"
